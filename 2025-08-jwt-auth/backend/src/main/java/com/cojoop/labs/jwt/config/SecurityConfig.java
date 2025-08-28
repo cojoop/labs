28361aa6049594d4875a79837311ca26ca570c6d
cojoop <tmdgus8779@gmail.com>
2025-08-28 15:50:56 +0900

    feat(backend): add SecurityConfig for API test

---
+++ 2025-08-jwt-auth/backend/src/main/java/com/cojoop/labs/jwt/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.cojoop.labs.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.ignoringRequestMatchers("/api/healthz"))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/healthz").permitAll()
+            .anyRequest().authenticated()
+        ).formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults());
+    return http.build();
+  }
+}
